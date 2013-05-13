75b7cb0c9580a645e85fe3f4e80c5ba31a317fc2
SVGN <sevgin.hashimov@gmail.com>
2013-05-13 22:25:02 +0300

    Added Utils.cs with SecretNumberGenerator()

---
+++ BullAndCows/BullsAndCows/Utils.cs
@@ -0,0 +1,23 @@
+﻿namespace BullsAndCows
+{
+    using System;
+
+    public static class Utils
+    {
+        public static int SecretNumberGenerator()
+        {
+            const int SecretNumberDigitsCount = 4;
+            const int DigitsCount = 10;
+            Random randomNumberGenerator = new Random();
+            int secretNumber = 0;
+
+            for (int i = 0; i < SecretNumberDigitsCount; i++)
+            {
+                int randomDigit = randomNumberGenerator.Next(0, DigitsCount);
+                secretNumber += Math.Pow(DigitsCount, i) * randomDigit;
+            }
+
+            return secretNumber;
+        }
+    }
+}
