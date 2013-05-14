e8665df841a0028eca56cab2d857c92603d4126b
Desislava Dimitrova <desi.pavlinova@gmail.com>
2013-05-15 00:46:20 +0300

    Added Player class unit tests

--- BullAndCows/BullsAndCows.Test/PlayerTests.cs
+++ BullAndCows/BullsAndCows.Test/PlayerTests.cs
@@ -7,8 +7,43 @@ namespace BullsAndCows.Test
     public class PlayerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PlayerConstructorTestName()
         {
+            string name = "Petar Ivanov";
+            int attempts = 6;
+            Player player = new Player(name, attempts);
+            Assert.AreEqual(player.Name, "Petar Ivanov");
+        }
+        [TestMethod]
+        public void PlayerConstructorTestAttempts()
+        {
+            string name = "Petar Ivanov";
+            int attempts = 6;
+            Player player = new Player(name, attempts);
+            Assert.AreEqual(player.Attempts, 6);
+        }
+        [TestMethod]
+        public void PlayerToStringTest()
+        {
+            string name = "Petar Ivanov";
+            int attempts = 6;
+            Player player = new Player(name, attempts);
+            string result = player.ToString();
+            Assert.AreEqual(result, string.Format("{0} --> {1} guesses", name, attempts));
+        }
+        [TestMethod]
+        public void PlayerCompareEqualTest()
+        {
+            Player player1 = new Player("Ivan", 3);
+            Player player2 = new Player("Ivan", 3);
+            Assert.AreEqual(player1.CompareTo(player2), 0);
+        }
+        [TestMethod]
+        public void PlayerCompareNotEqualTest()
+        {
+            Player player1 = new Player("Ivan", 3);
+            Player player2 = new Player("Dragan", 4);
+            Assert.AreNotEqual(player1.CompareTo(player2), 0);
         }
     }
 }
