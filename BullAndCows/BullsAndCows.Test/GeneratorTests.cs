98151108458213118e00d89a8adce68040e51706
Desislava Dimitrova <desi.pavlinova@gmail.com>
2013-05-15 01:06:29 +0300

    Added Generator SecretNumber method unit test

---
+++ BullAndCows/BullsAndCows.Test/GeneratorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Test
+{
+    [TestClass]
+    public class GeneratorTests
+    {
+        [TestMethod]
+        public void SecretNumberTest()
+        {
+            string[] randomNumbers = new string[10];
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = Generator.SecretNumber(4);
+            }
+
+            //We will check whether we have at least 2 different numbers in the array.
+            bool hasDifferentNumbers = false;
+            for (int i = 1; i < randomNumbers.Length; i++)
+            {
+                if (randomNumbers[0] != randomNumbers[i])
+                    hasDifferentNumbers = true;
+            }
+
+            if (!hasDifferentNumbers)
+                Assert.Fail("The generated numbers are not random.");                
+        }
+    }
+}
